e1fed1d0086fdfa04a99c7f3e92dee8d2ed32652
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-08-20 13:16:55 +0530

    alignment

--- src/components/Onboard.jsx
+++ src/components/Onboard.jsx
@@ -4,22 +4,22 @@ export default function FeatureSection() {
   return (
     <div className="px-4 py-8">
       {/* Heading */}
-      <h2 className="text-2xl font-semibold text-center mb-6">
-        Effortless <span className="text-blue-500">Onboarding</span> & Flexible Deployment
+      <h2 className="text-[24px] font-inter font-medium text-center mb-6">
+        Effortless <span className="text-[#1976D2]">Onboarding</span> & <br/>Flexible Deployment
       </h2>
 
       {/* Row: Left text + Right video */}
-      <div className="flex items-start justify-between gap-4 mb-6">
+      <div className="flex items-center justify-between gap-4  mb-6">
         {/* Left text */}
-        <div className="w-1/2">
-          <p className="text-gray-600 text-sm">
+        <div className="w-1/2 px-2">
+          <p className="text-[#000000B2] text-[14px]">
             Accqrate Books is fully cloud-native - no complex installations, no specialized IT needed. 
             Set up your accounting in minutes, not weeks.
           </p>
         </div>
 
         {/* Right video box */}
-        <div className="w-1/2 bg-white rounded-xl shadow-md p-3 flex flex-col justify-center">
+        <div className="w-1/2 bg-white text-center rounded-xl shadow-md p-3 flex flex-col justify-center">
           <video
             controls
             className="rounded-lg w-full h-32 object-cover"
@@ -29,9 +29,9 @@ export default function FeatureSection() {
           >
             <source src="videos/cloud-first.mp4" type="video/mp4" />
           </video>
-          <h3 className="text-sm font-medium">No special hardware</h3>
-          <p className="text-xs text-gray-600">
-            Access your books anywhere - your data is always secure.
+          <h3 className="text-sm font-medium">Cloud-first or On-Prem</h3>
+          <p className="text-xs text-gray-600 ">
+           Choose cloud for instant launch, or on-premises for full data control.
           </p>
         </div>
       </div>
